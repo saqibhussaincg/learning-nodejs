e772ab28596536f922f67cbb673fcde466fb1464
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-04-11 22:47:24 +0500

    'htmlextension'

---
+++ CODESTEPBYSTEP/expressintro/index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+
+
+
+
+// app.get('', (req, res) => {
+//     console.log('data send by browser ===>', req.query.name); // request browser se arahi hai eg: localhost:5000/?name=saqib
+//     res.send('<h2>welcome, home page </h2>' +  req.query.name) // recieving on browser
+// });
+
+
+
+// making href links
+// app.get('', (req, res) => {
+//     res.send(`
+//     <h2>welcome to Home Page</h2> <a href="/about"> about page link </a>
+//     `)
+// });
+
+// app.get('/about', (req, res) => {
+//     res.send(`
+//     <h2> This is about page </h2> <a href="/"> home page link </a>
+//     `)
+// });
+
+// app.get('/help', (req, res) => {
+//     res.send({
+//         name: 'saqib',
+//         email: 'saqib@gmail.com'
+//     })
+// });
+
+
+
+
+// ==== making calling HTML PAGES =====
+
+// calling another file from another folder or same folder
+
+const path = require('path');
+
+
+const publicpath = path.join(__dirname, '') // agar same foler mai file ho tou empty chorh denge 
+// bs fodler ka path de dia ab os k andar jitni files hongi sub ko access kar dega
+
+// app.use(express.static(publicpath));
+
+// page direct call karne k liye .html likhne ki zarurat nahi hogi ab
+app.get('', (req, res) => {
+    res.sendFile(`${publicpath}/index.html`)
+})
+app.get('/home', (req, res) => {
+    res.sendFile(`${publicpath}/home.html`)
+})
+app.get('/about', (req, res) => {
+    res.sendFile(`${publicpath}/about.html`)
+})
+
+// galat url dalne per * ka sign show hoga
+app.get('*', (req, res) => {
+    res.sendFile(`${publicpath}/errorpage.html`)
+})
+
+
+
+app.listen(5000);
--- CODESTEPBYSTEP/index.js
+++ CODESTEPBYSTEP/index.js
@@ -26,7 +26,7 @@
 
 // ------------ CORE MODULE ----------------
 
-const fs = require('fs');
+// const fs = require('fs');
 
 // file create karne k liye
 // fs.writeFileSync('hello.txt', ' learning node js ');
@@ -38,15 +38,31 @@ const fs = require('fs');
 
 // ------- Creating File Lists Multilple -----
 
-const path = require('path');
-const dirPath = path.join(__dirname, 'filelist');
+// const path = require('path');
+// const dirPath = path.join(__dirname, 'filelist');
 
 // for( i = 0; i < 5; i++) {
 //     fs.writeFileSync(dirPath+'/Hello'+i+'.txt','Hi');
 // }
 
-fs.readdir(dirPath, (err, item) => {
-    item.map( (items) => {
-        console.log('--->>>', items);
-    })
-})
+// fs.readdir(dirPath, (err, item) => {
+//     item.map( (items) => {
+//         console.log('--->>>', items);
+//     })
+// })
+
+// ==== making calling HTML PAGES =====
+
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const publicpath = path.join(__dirname, 'expressintro') // dosre folder ki file ko access karne k liye folder na naam de dia 
+app.use(express.static(publicpath));
+
+console.log(__dirname);
+app.listen(5000);
+
+
