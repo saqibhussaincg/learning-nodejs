9359e2a5c1276c47a4ece88cbda240312e9265d2
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-04-17 17:17:16 +0500

    'deletemethod'

--- CODESTEPBYSTEP/apitest/api.js
+++ CODESTEPBYSTEP/apitest/api.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.json());
 
 
+// instance mongo db ka delete karne k liye
+const mongoDb = require('mongodb');
+
+
 // is ko ek alag function mai bana kar bhi rakh sakte hain alag file mai 
 app.get('/', async (req, resp) => {
     let data = await dbConnect();
@@ -42,8 +46,38 @@ app.post('/', async (req, res) => {
 })
 
 
-app.put('/', (req, res) => {
+// postman se manualy data likh k send kardia ab isay get kese kar sakte hain
+// app.put('/:name', async (req, res) => {
+    // agar postman k param se change karna hai tou agar name mai same name hoga tou match hi nahi hoga tou postman  k param mai naam denge /S25 or req.param.name ho jayega
+
+      
+app.put('/', async (req, res) => {
+    // ab jese hi postman se send karenge tou wahan jo manualy body mai data likha tha wo console mai show kara dega
+    console.log(req.body);
+
+
+    let data = await dbConnect(); // connecting mongo
+
+    // result query likhenge dekhenge k successful data hua ya nahi
+
+    // update one function lega os k andar 2 object lega pehle mai condition hogi or 2nd mai jo updata karna chahte hain wo rahega
+    let result = data.updateOne(
+        {name : req.body.name}, // condition
+        { $set : req.body} // dynamic
+        // { $set : {price:900}} // static data
+    )
     res.send({result:"update"})
 })
+
+
+// DELETE API METHOD
+
+app.delete("/:id"), async (req,res) => {
+    console.log(req.params.id);
+    const data = await dbConnect();
+    const result = await data.deleteOne({_id: new mongoDb.ObjectId(req.params.id)})
+
+    res.send(result)
+}
 
 app.listen(5000);
